6961b1040afdc088bd4a6e29f31646ef30bbf456
TheHappyProgrammer0 <83089110+TheHappyProgrammer0@users.noreply.github.com>
2021-04-24 17:17:53 +0100

    Update Loop.py

--- src/Loop.py
+++ src/Loop.py
@@ -1,10 +1,9 @@
+import socket
 import pygame
 import Member
-from Init import (
-    init, screen
-)
 from Game import game
-import socket
+from Init import init, screen
+
 
 def Loop(in_game):
     while in_game:
@@ -12,11 +11,14 @@ def Loop(in_game):
             if event.type == pygame.QUIT:
                 in_game = False
                 pygame.quit()
-            if event.type == pygame.KEYDOWN:
-                if keys_pressed == pygame.K_UP:
-                    move.Up()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    in_game = False
+                    pygame.quit()
+                
+                elif event.key == pygame.K_UP:
+                    player.move(up)
                     
 if __name__ == "__main__":
     game.setUp()
-    keys_pressed = pygame.key.get_pressed()
     Loop(True)
