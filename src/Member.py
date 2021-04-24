2504bce06fc8f537933d34f1c895068f439eeb2e
TheHappyProgrammer0 <83089110+TheHappyProgrammer0@users.noreply.github.com>
2021-04-24 08:41:49 +0100

    Add files via upload

---
+++ src/Member.py
@@ -0,0 +1,10 @@
+import pygame
+import Init
+
+class Member:
+    def __init__(self, picturePath="/home/pi/"):
+        self.picturePath = picturePath
+        
+    def createPicture(self):
+        return 0
+                
