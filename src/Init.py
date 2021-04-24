6692516531c4c22e5f58648502ea4dab6b2526e7
TheHappyProgrammer0 <83089110+TheHappyProgrammer0@users.noreply.github.com>
2021-04-24 08:50:54 +0100

    Create Init.py

---
+++ src/Init.py
@@ -0,0 +1,22 @@
+import pygame
+
+WIDTH = 900
+HEIGHT = 1200
+
+screen = pygame.display
+
+class Init:
+  
+  global x
+  global y
+  
+  def Display(self):
+    display = screen.set_mode((WIDTH, HEIGHT) 
+    screen.set_caption("Genius Maths Farts Game") 
+    screem.flip()                          
+                              
+  def Vars(self, x=0, y=0):
+    x = 0
+    y = 0                         
+                            
+      
