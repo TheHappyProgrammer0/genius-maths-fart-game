b445d45db6c3b2fae7bed20ee13a1d57b06e113f
TheHappyProgrammer0 <83089110+TheHappyProgrammer0@users.noreply.github.com>
2021-04-24 17:17:22 +0100

    Update Init.py

--- src/Init.py
+++ src/Init.py
@@ -5,18 +5,25 @@ HEIGHT = 1200
 
 screen = pygame.display
 
+x = 0
+y = 0
+
+heath = 0
+
 class Init:
   
   global x
   global y
+  global health
   
   def Display(self):
-    display = screen.set_mode((WIDTH, HEIGHT) 
+    display = screen.set_mode((WIDTH, HEIGHT))
     screen.set_caption("Genius Maths Farts Game") 
-    screem.flip()                          
+    screen.flip()                          
                               
   def Vars(self, x=0, y=0):
+    health = 100
     x = 0
     y = 0                         
                             
-      
+init = Init()
