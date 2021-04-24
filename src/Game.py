58c7163067a9c32dfde90c3132c6bef173bf0739
TheHappyProgrammer0 <83089110+TheHappyProgrammer0@users.noreply.github.com>
2021-04-24 17:16:12 +0100

    Update Game.py

--- src/Game.py
+++ src/Game.py
@@ -1,11 +1,27 @@
 from Init import (
-    init
-)              
-from Move import (
-    move
-)      
+    init, x, y 
+)           
+from Move import move 
+from random import randrange
 
 class Game:
+    class Player:
+        def __init__(self, x, y, health=100):
+            self.x = x
+            self.y = y
+            self.health = health
+            self.move = move
+            
+        def Move(self, dir):
+            if self.dir == up:
+                self.dir == move.Up()
+            
+        def checkCollision(self, x, y):
+            return 0
+        
+        def checkHealth(self):
+            return 0     
+                
     def setUp(self):
         init.Display()
         init.Vars()
