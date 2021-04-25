cad7bab78c9678e0aecaee14c161a44ab9782ec3
TheHappyProgrammer0 <83089110+TheHappyProgrammer0@users.noreply.github.com>
2021-04-25 07:58:01 +0100

    Update Init.py

--- src/Init.py
+++ src/Init.py
@@ -1,29 +1,16 @@
 import pygame
+from random import randrange
 
 WIDTH = 900
 HEIGHT = 1200
 
 screen = pygame.display
+display = pygame.display.set_mode((WIDTH, HEIGHT))
+screen.set_caption("Genius Maths Farts Game")
 
 x = 0
 y = 0
 
-heath = 0
+health = 100
 
-class Init:
-  
-  global x
-  global y
-  global health
-  
-  def Display(self):
-    display = screen.set_mode((WIDTH, HEIGHT))
-    screen.set_caption("Genius Maths Farts Game") 
-    screen.flip()                          
-                              
-  def Vars(self, x=0, y=0):
-    health = 100
-    x = 0
-    y = 0                         
-                            
-init = Init()
+facingAngle = randrange(0, 365)      
